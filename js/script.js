992b3950eb9062fabf112bdf9f674e7862c4bba5
leticea <nascimento.leticeah@gmail.com>
2022-12-07 17:52:05 -0300

    criando função de click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 const yourVoteFor = document.querySelector('.text span');
 const role = document.querySelector('.role span');
-const description= document.querySelector('.description');
+const description = document.querySelector('.description');
 const warning = document.querySelector('.bottom-side');
+const photos = document.querySelector('.top-side-right');
+const numbers = document.querySelector('.numbers');
 
+function clicked(num) {
+    alert("Clicou em "+num);
+}
+
+function empty() {
+    alert("Clicou em branco");
+}
+
+function correct() {
+    alert("Clicou em corrige");
+}
+
+function confirm() {
+    alert("Clicou em confirma");
+}
