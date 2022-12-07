e88126b3f28e23e95992f37c779e0dea98cb725e
leticea <nascimento.leticeah@gmail.com>
2022-12-07 17:09:55 -0300

    inserindo as etapas

---
+++ js/phases.js
@@ -0,0 +1,50 @@
+let phases = [
+    {
+        title: 'VEREADOR',
+        numbers: 5,
+        candidates: [
+            {
+                number: 38111,
+                name: 'Fulano de Tal',
+                party: 'ABC',
+                photos: [
+                    {url:'pessoa-passaro.png', subtitle: 'Vereador'}
+                ]
+            },
+            {
+                number: 77222,
+                name: 'Beltrano da Silva',
+                party: 'DEBUG',
+                photos: [
+                    {url:'scroopy.png', subtitle: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        title: 'PREFEITO',
+        numbers: 2,
+        candidates: [
+            {
+                number: 66,
+                name: 'Ciclano',
+                party: 'ABC',
+                vice: 'Cic',
+                photos: [
+                    {url:'rick.png', subtitle: 'Prefeito'},
+                    {url:'summer.png', subtitle: 'Vice-Prefeita', small: true}
+                ]
+            },
+            {
+                number: 33,
+                name: 'Zulano',
+                party: 'QUERY',
+                vice: 'Zul',
+                photos: [
+                    {url:'beth.png', subtitle: 'Prefeita'},
+                    {url:'jessica.png', subtitle: 'Vice-Prefeita', small: true}
+                ]
+            },
+        ]
+    }
+];
