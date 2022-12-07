d61eecb1fa4b6ec8da31f0bf119e3071405ac2c4
leticea <nascimento.leticeah@gmail.com>
2022-12-07 17:26:26 -0300

    separando as const no js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const yourVoteFor = document.querySelector('.text span');
+const role = document.querySelector('.role span');
+const description= document.querySelector('.description');
+const warning = document.querySelector('.bottom-side');
+
